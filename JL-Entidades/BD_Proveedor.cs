0a3676efedd44621c4059734e0f835a991189262
Sebastian Villagran <SebastianVillagran13@hotmail.com>
2020-12-07 12:13:59 -0600

    Creacion de entiad de proveedor

---
+++ JL-Entidades/BD_Proveedor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JL_Entidades
+{
+  public class BD_Proveedor
+    {
+      public int idProvee { get; set; }
+      public String nombre { get; set; }
+      public String direccion { get; set; }
+      public String telefono { get; set; }
+      public String rubro { get; set; }
+      public String rfc { get; set; }
+      public String correo { get; set; }
+      public String contacto { get; set; }
+      public String foto_logo { get; set; }
+      public String estado_provdr { get; set; }
+
+        public BD_Proveedor()
+        {
+        }
+
+        public BD_Proveedor(int idProvee, string nombre, string direccion, string telefono, string rubro, string rfc,
+            string correo, string contacto, string foto_logo, string estado_provdr)
+        {
+            this.idProvee = idProvee;
+            this.nombre = nombre;
+            this.direccion = direccion;
+            this.telefono = telefono;
+            this.rubro = rubro;
+            this.rfc = rfc;
+            this.correo = correo;
+            this.contacto = contacto;
+            this.foto_logo = foto_logo;
+            this.estado_provdr = estado_provdr;
+        }
+    }
+}
--- Jolugago-Project1/Login.Designer.cs
+++ Jolugago-Project1/Login.Designer.cs
@@ -29,12 +29,29 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 5;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
     }
 }
 
